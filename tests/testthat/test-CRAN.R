10e0870ca10f8059074f8d1d411805cf9e116269
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2024-09-23 16:32:38 -0400

    atime_pkg_test_info returns env with test.call

--- tests/testthat/test-CRAN.R
+++ tests/testthat/test-CRAN.R
@@ -416,7 +416,19 @@ test_that("error for new unit name conflicting with existing", {
 
 test_that("pkg.edit.fun is a function", {
   example_tests.R <- system.file("example_tests.R", package="atime")
-  test.env <- atime:::test_file_to_env(example_tests.R)
+  tests.dir <- file.path(tempfile(), ".ci", "atime")
+  dir.create(tests.dir, showWarnings = FALSE, recursive = TRUE)
+  tests.R <- file.path(tests.dir, "tests.R")
+  file.copy(example_tests.R, tests.R)
+  ci.dir <- dirname(tests.dir)
+  pkg.dir <- dirname(ci.dir)
+  DESCRIPTION <- file.path(pkg.dir, "DESCRIPTION")
+  cat("Package: atime\nVersion: 1.0\n", file=DESCRIPTION)
+  git2r::init(pkg.dir)
+  repo <- git2r::repository(pkg.dir)
+  git2r::add(repo, DESCRIPTION)
+  git2r::commit(repo, "test commit")
+  test.env <- atime::atime_pkg_test_info(pkg.dir)
   test_N_expr <- test.env$test.list$test_N_expr
   expect_identical(test_N_expr$pkg.edit.fun, test.env$edit.data.table)
   expect_identical(test_N_expr$N, 2)
