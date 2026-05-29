f5a258b47c91555ffa4da7a410fac3d2766c4120
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-05-29 04:42:00 -0400

    atime_pkg errors

--- tests/testthat/test-versions.R
+++ tests/testthat/test-versions.R
@@ -137,13 +137,15 @@ test_that("pkg.edit.fun is a function", {
   test_N_expr <- test.env$test.list$test_N_expr
   expect_identical(test_N_expr$pkg.edit.fun, test.env$edit.data.table)
   expect_identical(test_N_expr$N, c(2,20))
-  expect_identical(test_N_expr$expr, quote(rnorm(N)))
+  expect_identical(test_N_expr$expr, quote(atime:::.packageName))
   test_expr <- test.env$test.list$test_expr
   expect_identical(test_expr$pkg.edit.fun, test.env$edit.data.table)
   expect_identical(test_expr$N, c(9,90))
-  expect_identical(test_expr$expr, quote(rnorm(N)))
+  expect_identical(test_expr$expr, quote(atime:::.packageName))
   e.res <- eval(test.env$test.call[["global_var_in_setup"]])
   expect_is(e.res, "atime")
+  p.res <- atime::atime_pkg(pkg.dir)
+  expect_is(p.res, "list")
 })
 
 gdir <- tempfile()
