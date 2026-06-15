2970f3c2a09c1b0316fb3874d90c8c87e2c34cda
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-06-15 01:02:56 -0400

    master

--- tests/testthat/test-versions.R
+++ tests/testthat/test-versions.R
@@ -183,7 +183,10 @@ if(interactive())test_that("atime_pkg() works for poncatime", {
   poncatime_dir <- tempfile()
   dir.create(poncatime_dir)
   gert::git_clone("https://github.com/poncateam/poncatime", poncatime_dir)
-  gert::git_branch_create("atime-version-test", "afe6f986836f3f37e743bda93a8d0746587aad1a", repo=poncatime_dir)
-  poncatime_info <- atime::atime_pkg(poncatime_dir)
-  expect_equal(nrow(poncatime_info$all), 2)#two test cases run.
+  gert::git_branch_checkout("pkg.edit.fun", repo=poncatime_dir)
+  system(paste("cd", poncatime_dir, "&& git submodule update --init --recursive && cd src/external/ponca && git checkout master"))
+  tinfo <- atime::atime_pkg_test_info(poncatime_dir)
+  expect_identical(tinfo$HEAD.name, "HEAD=master")
+  ##poncatime_info <- atime::atime_pkg(poncatime_dir)
+  ##expect_equal(nrow(poncatime_info$all), 2)#two test cases run.
 })
